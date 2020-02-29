e7fb4ccf6e6ffb0507c28e5c5ce5098715657f1b
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-28 16:46:43 -0800

    Add method to generate hash value out of user input.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,28 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      inputData: ''
+    }
+  }
   _userInput = React.createRef()
+
+  generateHashValue = () => {
+    const data = 'QNimate'
+    const crypto = window.crypto || window.msCrypto
+    if(crypto.subtle) {
+      console.log("Cryptography API supported")
+
+      const promise = crypto.subtle.digest({name: "SHA-256"}, this.convertStringToArrayBuffer(data))
+      promise.then(result => {
+        const hash_value = this.convertArrayBufferToHexaDecimal(result)
+      })
+    } else {
+      console.log("Cryptography API not supported.")
+    }
+  }
 
   convertStringToArrayBuffer = (e) => {
     e.preventDefault()
@@ -11,14 +32,26 @@ class App extends Component {
     for(let iii = 0; iii < this._userInput.length; iii++) {
       bytes[iii] = this._userInput.charCodeAt(iii)
     }
-    
     console.log('ARRAY BUFFER', bytes)
+  }
+
+  convertArrayBufferToHexaDecimal = (buffer) => {
+    let data_view = new DataView(buffer)
+    let iii, len, hex = '', c;
+    for(iii = 0, len = data_view.byteLength; iii < len; iii++) {
+      c = data_view.getUint8(iii).toString(16)
+      if(c.length < 2) {
+        c += '0'
+      }
+      hex += c
+    }
+    console.log('HEX', hex)
   }
 
   render() {
     return (
       <div className="App">
-        <form onSubmit={this.convertStringToArrayBuffer}>
+        <form onSubmit={this.generateHashValue}>
           <input
             type='text'
             name='userInput'

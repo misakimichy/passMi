d8715b93b21dade46df6da0ce5e27f426dd72780
Misaki Koonce <misaki.koonce@gmail.com>
2020-03-06 12:17:07 -0800

    Add decrypt the ciphertext method.

--- src/components/EncryptForm.js
+++ src/components/EncryptForm.js
@@ -3,7 +3,6 @@ import crypto from 'crypto'
 
 class EncryptForm extends Component {
     state = {
-        data: 'QNimate',
         userInput: '',
         KEY: '',
         decipher: ''
@@ -13,19 +12,17 @@ class EncryptForm extends Component {
         // Initialization Vector - 16 bytes
         const iv = new Buffer(crypto.randomBytes(16), 'utf8')
         const cipher = crypto.createCipheriv('aes-256-gcm', key, iv)
-
         let encoded = cipher.update(input, 'utf8', 'base64')
         encoded += cipher.final('base64')
         return [encoded, iv, cipher.getAuthTag()]
     }
 
     decryptMessage(key, encoded, iv, authTag) {
-        const decipher = crypto.createDecipheriv('aes-2560-gcm', key, iv)
+        const decipher = crypto.createDecipheriv('aes-256-gcm', key, iv)
         decipher.setAuthTag(authTag)
         let text = decipher.update(encoded, 'base64', 'utf8')
         text += decipher.final('utf8')
-
-        console.log(text)
+        return text
     }
 
     /*
@@ -35,13 +32,13 @@ class EncryptForm extends Component {
         event.preventDefault()
         const KEY = new Buffer(crypto.randomBytes(32), 'utf8')
         // const aesCipher = this.aes256gcm(KEY)
-        const encrypt = this.encryptMessage(this.state.userInput, KEY)
-        console.log('encoded', encrypt[0])
-        console.log('iv', encrypt[1])
-        console.log('cipher text', encrypt[2])
-        return(
-            encrypt.map(elem => <p>{elem}</p>)
-        )
+        const [encrypted, iv, authTag] = this.encryptMessage(this.state.userInput, KEY)
+        console.log('encoded', encrypted)
+        console.log('iv', iv)
+        console.log('cipher text', authTag)
+
+        const decrypted = this.decryptMessage(KEY, encrypted, iv, authTag)
+        console.log('DECRYPtED MESSAGE:', decrypted)
         // const decrypt = this.decryptMessage(encrypted, iv, authTag)
         // this.setState({ decipher: decrypted})
     }

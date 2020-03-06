098de8fc5f3fae48313ce3c5047b8de9a5f0633b
Misaki Koonce <misaki.koonce@gmail.com>
2020-03-06 14:56:49 -0800

    Delete Decrypt Form.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import './App.css'
 import EncryptForm from './EncryptForm'
-import DecryptForm from './DecryptForm'
 
 const App = () => {
   return (
     <div className="App">
       <EncryptForm />
-      {/* <DecryptForm /> */}
     </div>
   )
 }
